b9bec81208ce9f05428807f812703b4a27e55374
David-zaiwang <71278867+David-zaiwang@users.noreply.github.com>
2021-08-27 00:03:36 +0800

    add dice and mask_iou metric
    add dice and mask_iou metric

--- Metrics.py
+++ Metrics.py
@@ -1,6 +1,8 @@
 # *coding:utf-8 *
 
 import sklearn.metrics as metrics
+import numpy as np
+
 
 def calculate_auc_test(prediction, label):
     # read images
@@ -39,3 +41,69 @@ def accuracy(pred_mask, label):
     spe = TN / (TN + FP)
 
     return acc, sen, spe
+
+
+def mask_iou(mask1, mask2):
+    mask1_area = np.count_nonzero(mask1 == 1)  # I assume this is faster as mask1 == 1 is a bool array
+    mask2_area = np.count_nonzero(mask2 == 1)
+    intersection = np.count_nonzero(np.logical_and(mask1, mask2))
+    iou = intersection / (mask1_area + mask2_area - intersection)
+    return iou
+
+
+def dice(im1, im2, empty_score=1.0):
+    """
+    This code is from https://gist.github.com/brunodoamaral/e130b4e97aa4ebc468225b7ce39b3137
+    Computes the Dice coefficient, a measure of set similarity.
+    Parameters
+    ----------
+    im1 : array-like, bool
+        Any array of arbitrary size. If not boolean, will be converted.
+    im2 : array-like, bool
+        Any other array of identical size. If not boolean, will be converted.
+    Returns
+    -------
+    dice : float
+        Dice coefficient as a float on range [0,1].
+        Maximum similarity = 1
+        No similarity = 0
+        Both are empty (sum eq to zero) = empty_score
+
+    Notes
+    -----
+    The order of inputs for `dice` is irrelevant. The result will be
+    identical if `im1` and `im2` are switched.
+    """
+    im1 = np.asarray(im1).astype(np.bool)
+    im2 = np.asarray(im2).astype(np.bool)
+
+    if im1.shape != im2.shape:
+        raise ValueError("Shape mismatch: im1 and im2 must have the same shape.")
+
+    im_sum = im1.sum() + im2.sum()
+    if im_sum == 0:
+        return empty_score
+
+    # Compute Dice coefficient
+    intersection = np.logical_and(im1, im2)
+
+    return 2. * intersection.sum() / im_sum
+
+
+if __name__ == '__main__':
+    print("Testing the dice coefficient")
+    a = np.array([[0, 0, 0, 0],
+                  [0, 1, 1, 0],
+                  [0, 1, 1, 0],
+                  [0, 0, 0, 0]])
+
+    b = np.array([[0, 0, 0, 0],
+                  [0, 0, 1, 1],
+                  [0, 0, 1, 1],
+                  [0, 0, 0, 0]])
+
+    dice_coe = dice(a, b)
+    print("dice is ", dice_coe)
+
+    mask_iou = mask_iou(a, b)
+    print("mask iou is ", mask_iou)

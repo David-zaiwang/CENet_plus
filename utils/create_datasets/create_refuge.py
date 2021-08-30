7e96b814910148eb497aaa441ed9614762f0b512
顾在旺 <zaiwang.gu@ubtrobot.com>
2021-08-30 16:03:54 +0800

    add create_refuge.py

--- utils/create_datasets/create_refuge.py
+++ utils/create_datasets/create_refuge.py
@@ -1,1 +1,12 @@
 # *coding:utf-8 *
+
+"""
+This code is to create cropped optic disc dataset from the original refuge dataset.
+We crop the disc image with the size of 800 * 800.
+"""
+
+import os
+import cv2
+import shutil
+
+image_folder =
